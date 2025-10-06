af1fd2c0a29e6d4ec1099ec0f7fdb8b16dd63b6b
M4ds12 <madsnfjelsted@gmail.com>
2025-10-06 13:30:17 +0200

    lister-lavet

--- 01_web_template/index.js
+++ 01_web_template/index.js
@@ -6,39 +6,19 @@ var currentPage = "#page10" //Hvilken side er aktiv
 function setup() {
     console.log("P5 setup kaldt")
     //Sæt event listeners op på menu
-    select("#menu-side1").mousePressed(
-        function(){shiftPage("#page1")}
-    )
-    select("#menu-side2").mousePressed(
-        function(){shiftPage("#page2")}
-    )
-    select("#menu-side3").mousePressed(
-        function(){shiftPage("#page3")}
-    )
-    select("#menu-side4").mousePressed(
-        function(){shiftPage("#page4")}
-    )
-    select("#menu-side5").mousePressed(
-        function(){shiftPage("#page5")}
-    )
-    select("#menu-side6").mousePressed(
-        function(){shiftPage("#page6")}
-    )
-    select("#menu-side7").mousePressed(
-        function(){shiftPage("#page7")}
-    )
-    select("#menu-side8").mousePressed(
-        function(){shiftPage("#page8")}
-    )
-    select("#menu-side9").mousePressed(
-        function(){shiftPage("#page9")}
-    )
-    select("#menu-side10").mousePressed(
-        function(){shiftPage("#page10")}
-    )
-    select("#menu-side11").mousePressed(
-        function(){shiftPage("#page11")}
-    )
+    //variablet AllMenuItem, selecter alle a elementer under klassen sidebar
+   var allMenuItem = selectAll(".sidebar a")
+   console.log(allMenuItem)
+   allMenuItem.map(
+    function(item){
+        item.mousePressed(
+            function(){
+                shiftPage(item.attribute("action"))
+            }
+        )
+    }
+
+)
 }
 
 function shiftPage(newPage){
