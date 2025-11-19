caa2a0d1fb3608bfd6f81345a9ed61eb4a5e0780
M4ds12 <madsnfjelsted@gmail.com>
2025-11-19 23:08:54 +0100

    rdr2-opgave

---
+++ 00_rdr2_opgave/index.js
@@ -0,0 +1,50 @@
+//Globale variabler
+var currentPage = "#page1" //Hvilken side er aktiv
+
+//P5 Setup() bliver kaldt EN gang før siden vises
+
+
+
+
+function setup() {
+    console.log("P5 setup kaldt")
+    //Sæt menu op
+    //Trin 1: jeg laver en liste og sætter et array op ved at hente alle siderne
+    var allPages = selectAll(".page")
+    
+    //skift til current page
+    shiftPage(currentPage);
+
+
+    var myButton = createButton("Button created with javascript")
+   //Læg en ind i side 5
+   select("#theButton").child(myButton)
+   //Lav en event listener
+   myButton.mousePressed(()=>{
+    shiftPage("#page2")
+   })
+
+     allPages.map(
+        page => {
+            //Lav et nyt "a" element
+            var menuItem = createElement("a")
+            //Sæt a taggets html til sidens titel
+            menuItem.html(page.attribute("title"))
+            //sæt a tagget ind i sidebaren
+            select(".sidebar").child(menuItem)
+            //sæt event listener på a tagget
+            menuItem.mousePressed(
+                ()=>{
+                    shiftPage("#" + page.attribute("id"))
+                }
+            )
+        }
+   )
+}
+
+function shiftPage(newPage){
+    select(currentPage).removeClass("show")
+    select(newPage).addClass("show")
+    currentPage = newPage;
+}
+
