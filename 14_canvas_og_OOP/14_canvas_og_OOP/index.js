3d1e785b4093be913abb2cd1aadc76de53d96034
M4ds12 <madsnfjelsted@gmail.com>
2026-09-14 15:15:43 +0200

    aa

--- 14_canvas_og_OOP/14_canvas_og_OOP/index.js
+++ 14_canvas_og_OOP/14_canvas_og_OOP/index.js
@@ -1,25 +1,146 @@
-var gravity 
-var friction  
+var gravity
+var friction
 var b
+var points = 10
+var bSound
+var gameOver = false
+var timerInterval = null
+var seconds = 0
+var floatingBalls = []
+var spawnInterval = 90
+var frameCounter = 0
 
-function setup() {
-  createCanvas(windowWidth, windowHeight)
-  gravity = createVector(0, 0.5)
-  friction = 0.99
 
-  b = new Ball(width/2, 0, 24, "orange", 12)
+
+async function setup() {
+    bSound = await loadSound("/api_lib/sounds/beep.mp3")
+    var c = createCanvas(windowWidth, windowHeight)
+    select('#page2').child(c)
+    select('#startButton').mousePressed(() => {
+        getAudioContext().resume()
+        shiftPage('#page2')
+        startTimer()
+    })
+
+
+    gravity = createVector(0, 0.5)
+    friction = 0.99
+
+    select('#infoPoints').html(points)
+
+    b = new Ball(windowWidth / 2, 0, 100, "orange", 12)
+    //f = new FloatingBall(100, 100, 50, "lightblue", 0, 12)
+
+
+    select('#restartButton').mousePressed(() => {
+        points = 10
+        gameOver = false
+        b.position = createVector(windowWidth / 2, 0)
+        b.velocity = createVector(0, 0)
+        shiftPage('#page2')
+        startTimer()
+    })
+
 }
 
 function draw() {
-  background(100)
-  b.update()
-  b.constrain()
-  b.show()
+    if (gameOver) return
+    background(100)
+
+    b.update()
+    b.constrain()
+    b.show()
+
+   /* if (b.hit(f)) {
+        if (!b.wasHit) {
+            points--
+            try {
+                bSound.play()
+            } catch (e) {
+                console.log("sound not ready yet:", e)
+            }
+            b.wasHit = true
+        }
+    } else {
+        b.wasHit = false
+    } */
+
+    select('#infoPoints').html(points)
+
+    //f.update()
+    //f.constrain()
+    //f.show()
+
+    frameCounter++
+if(frameCounter >= spawnInterval){
+  spawnFloatingBall()
+  frameCounter = 0
+  spawnInterval = random(60, 150)
 }
 
-function keyPressed(){
-  if(key == " "){
-    b.jump()
+for(var i = floatingBalls.length - 1; i >= 0; i--){
+  var fb = floatingBalls[i]
+  fb.update()
+  fb.show()
+
+  if(b.hit(fb)){
+    if(!fb.wasHit){
+      points--
+      try {
+        bSound.play()
+      } catch(e) {
+        console.log("sound skipped:", e.message)
+      }
+      fb.wasHit = true
+    }
+  }
+
+  if(fb.isOffScreen()){
+    floatingBalls.splice(i, 1)
   }
 }
 
+
+
+    if (points <= 0) {
+        gameOver = true
+        stopTimer()
+        shiftPage('#page3')
+        select("#stats").html("Du varede " + seconds + " sekunder")
+        
+    }
+
+}
+
+function keyPressed() {
+    if (key == " ") {
+        b.jump()
+        //f.jump()
+    }
+}
+
+
+function startTimer() {
+    seconds = 0
+    timerInterval = setInterval(() => {
+        seconds++
+        select('#timer').html(seconds + ' sek')
+    }, 1000)
+}
+
+function stopTimer() {
+    clearInterval(timerInterval)
+}
+
+function spawnFloatingBall(){
+  var r = random(30, 150)
+  var y = random(r, height - r)
+  var speed = random(8, 30)
+
+  var fromLeft = random() < 0.5
+  var x = fromLeft ? -r : windowWidth + r
+  var vel = fromLeft ? speed : -speed
+
+  var newBall = new SideBall(x, y, r, "lightblue", 0, vel)
+  floatingBalls.push(newBall)
+}
--- api_lib/myClasses.js
+++ api_lib/myClasses.js
@@ -1,28 +1,86 @@
 class Ball {
-  constructor(x, y, r, col, jump){ 
-    this.radius = r
-    this.col = col
-    this.velocity = createVector(0, 0)
-    this.position = createVector(x, y)
-    this.jumpForce = jump
-  }
-  update(){
-    this.velocity.add(gravity)
-    this.velocity.y *= friction 
-    this.position.add(this.velocity)
-  }
-  constrain(){
-    if(this.position.y > height - this.radius/2){
-      this.position.y = height - this.radius/2
-      this.velocity.y *= -1
+    constructor(x, y, r, col, jump) {
+        this.diam = r
+        this.col = col
+        this.velocity = createVector(0, 0)
+        this.position = createVector(x, y)
+        this.jumpForce = jump
+        this.wasHit = false
     }
-  }
-  jump(){
-    this.velocity.y -= this.jumpForce
-  }
-  
-  show(){
-    fill(this.col)
-    circle(this.position.x, this.position.y, this.radius)
-  }
+    update() {
+        this.velocity.add(gravity)
+        this.velocity.y *= friction
+        this.position.add(this.velocity)
+    }
+    constrain() {
+        if (this.position.y > height - this.diam / 2) {
+            this.position.y = height - this.diam / 2
+            this.velocity.y *= -1
+        }
+
+         //Sørg for at bolden kun går halvt ud af skærmen på toppen
+        if (this.position.y < this.diam / 2) {
+            this.position.y = this.diam / 2
+            this.velocity.y *= -1
+        }
+
+    }
+    jump() {
+        this.velocity.y -= this.jumpForce
+    }
+
+    show() {
+        fill(this.col)
+        circle(this.position.x, this.position.y, this.diam)
+    }
+    hit(anotherBall) {
+        var b = anotherBall
+        var totalR = (this.diam + b.diam) / 2
+        var d = dist(this.position.x, this.position.y, b.position.x, b.position.y)
+
+        if (d <= totalR) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class FloatingBall extends Ball {
+    constructor(x, y, r, col, jump, speed) {
+        //super betyder at vi overtager disse argumenter fra "super" klassen (Ball)
+        super(x, y, r, col, jump)
+        //vi overskriver velocity vektoren med en lokal der flytter sig på x aksen 
+        this.velocity = createVector(speed, 0)
+    }
+    update() {
+        this.position.add(this.velocity)
+    }
+
+    constrain() {
+        //sørg for at floatingball bouncer på siderne
+        this.position.x = constrain(this.position.x, this.diam / 2, windowWidth - this.diam / 2)
+
+        if (this.position.x <= this.diam / 2 || this.position.x >= windowWidth - this.diam / 2) {
+            this.velocity.mult(-1)
+        }
+
+       
+
+    }
+
+}
+
+
+class SideBall extends Ball {
+    constructor(x, y, r, col, jump, speed) {
+        super(x, y, r, col, jump)
+        this.velocity = createVector(speed, 0)
+    }
+    update() {
+        this.position.add(this.velocity)
+    }
+    isOffScreen() {
+        return this.position.x < -this.diam || this.position.x > windowWidth + this.diam
+    }
 }
